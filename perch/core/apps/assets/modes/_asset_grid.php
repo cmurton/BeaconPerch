4ac3b1fd077bf55179279aa512d3d98875478126
Chris <Chris>
2017-04-03 21:38:28 +0100

    fixed untracked files

--- perch/core/apps/assets/modes/_asset_grid.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	echo $HTML->open('div.asset-grid');
-
-	foreach($assets as $Asset) { 
-		echo PerchAssets_Display::grid_item($Asset, $HTML);
-	}
-
-	echo $HTML->close('div');
--- perch/core/assets/js/version.php
+++
@@ -1,1 +0,0 @@
-<?php define('PERCH_ASSET_VERSION', 'c4c70b856b16830782ef');
